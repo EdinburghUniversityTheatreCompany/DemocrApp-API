31735824f148e18003f308c2fe56b9153b5b4812
Kyle Cooke <cookekyle99@yahoo.co.uk>
2018-02-18 13:24:56 +0000

    starting place

---
+++ Meeting/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from jsonfield import JSONField
+
+
+class Meeting(models.Model):
+    time = models.DateTimeField
+    name = models.CharField
+
+
+class TokenSet(models.Model):
+    meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE)
+
+
+class Device(models.Model):
+    expires = models.DateTimeField
+    fingerprint = models.CharField
+
+
+class Token(models.Model):
+    token_set = models.ForeignKey(TokenSet, on_delete=models.CASCADE)
+    device = models.ForeignKey(Device, on_delete=models.DO_NOTHING)
+    creator = models.CharField
+    proxy = models.BooleanField(default=False)
+
+
+class Vote(models.Model):
+    token_set = models.ForeignKey(TokenSet, on_delete=models.CASCADE)
+    name = models.CharField
+    method = models.CharField
+
+
+class BallotEntry(models.Model):
+    token = models.ForeignKey(Token, on_delete=models.DO_NOTHING)
+    vote = models.ForeignKey(Vote, on_delete=models.CASCADE)
+    data = JSONField()
