b4f593b6a4c73b005fd65cdf792853fa88efadb0
Kyle Cooke <cookekyle99@yahoo.co.uk>
2018-02-18 18:21:13 +0000

    added channels

--- democrapp_api/settings.py
+++ democrapp_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'democrapp_api.wsgi.application'
+ASGI_APPLICATION = "democrapp_api.routing.application"
 
 
 # Database
