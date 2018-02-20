a20dd3f8293bcdeb59454f992371d6ad1c91c077
Kyle Cooke <cookekyle99@yahoo.co.uk>
2018-02-20 18:20:46 +0000

    more openstv tie-in

--- Meeting/tallying.py
+++ Meeting/tallying.py
@@ -1,9 +1,14 @@
+from threading import Thread
+
 from .models import Vote, BallotEntry, Option
 from openstv.ballots import Ballots
+from openstv.MethodPlugins.ScottishSTV import ScottishSTV
+from time import sleep
 
 def runOpenSTV(vote_id):
     ballots = Ballots()
     vote = Vote.objects.get(pk=vote_id)
+
     options = vote.option_set.all().order_by('pk')
     option_translation = {}
     names = []
@@ -11,6 +16,7 @@ def runOpenSTV(vote_id):
         option_translation[option.id] = index
         names.append(option.name)
     ballots.setNames(names)
+
     voter = -1
     ballot = []
     for be in BallotEntry.objects.filter(candidate__vote=vote).order_by('token_id', 'value').all():
@@ -19,5 +25,20 @@ def runOpenSTV(vote_id):
         voter = be.token_id
         ballot.append(option_translation[be.id])
 
+    electionCounter = ScottishSTV(ballots)
+    countThread = Thread(target=electionCounter.runElection)
+    countThread.start()
+    while countThread.isAlive():
+        sleep(0.1)
+        if not electionCounter.breakTieRequestQueue.empty():
+            [tiedCandidates, names, what] = electionCounter.breakTieRequestQueue.get()
+            c = askUserToBreakTie(tiedCandidates, names, what)
+            electionCounter.breakTieResponseQueue.put(c)
+        if "R" in vars(electionCounter):
+            status = "Counting votes using %s\nRound: %d" % \
+                     (electionCounter.longMethodName, electionCounter.R + 1)
+        else:
+            status = "Counting votes using %s\nInitializing..." % \
+                     electionCounter.longMethodName
 
 
