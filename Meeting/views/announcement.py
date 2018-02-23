beba66535b3ad867c6832d07e5644efc02768e65
kyle <cookekyle97@gmail.com>
2018-02-23 23:38:31 +0000

    fixed a bug in announcement

--- Meeting/views/announcement.py
+++ Meeting/views/announcement.py
@@ -3,11 +3,14 @@ from channels.layers import get_channel_layer
 from django.contrib.auth.decorators import login_required, permission_required
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+
 from ..models import Meeting
 
 
-@login_required(login_url='/admin/login')
-@permission_required('meeting.can_create')
+#@login_required(login_url='/admin/login')
+#@permission_required('meeting.can_create')
+@csrf_exempt
 def announcement(request, meeting_id):
     meeting = get_object_or_404(Meeting, pk=meeting_id)
     if not meeting.open():
@@ -18,5 +21,5 @@ def announcement(request, meeting_id):
                              "error": "no message"})
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)("broadcast", {"type": "announcement",
-                                                          "message": request["message"]})
+                                                          "message": request.POST["message"]})
     return JsonResponse({"result": "success"})
