ba4f1384e9fa8eeb252b6a76ed5d926175564192
Kyle Cooke <cookekyle99@yahoo.co.uk>
2018-02-23 17:49:48 +0000

    enabling messing with records in admin
    this should be un done later

--- Meeting/admin.py
+++ Meeting/admin.py
@@ -7,5 +7,19 @@ admin.site.register(Meeting)
 
 class AuthTokenAdmin(admin.ModelAdmin):
     fields = ['has_proxy', 'token_set']
+
+
+class OptionInline(admin.StackedInline):
+    model = Option
+    extra = 3
+
+
+class VoteAdmin(admin.ModelAdmin):
+    inlines = [OptionInline]
+
+
 admin.site.register(AuthToken, AuthTokenAdmin)
 admin.site.register(TokenSet)
+admin.site.register(Vote, VoteAdmin)
+admin.site.register(Option)
+admin.site.register(Tie)
