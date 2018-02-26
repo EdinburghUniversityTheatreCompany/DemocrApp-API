303b554cf262db504c40bdb5eb8d4b567fa8f95a
Kyle Cooke <cookekyle99@yahoo.co.uk>
2018-02-26 18:18:54 +0000

    starting to write tests

--- Meeting/tests.py
+++ Meeting/tests.py
@@ -1,3 +1,94 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.serializers import json
+from django.test import TestCase, Client
+from django.urls import reverse
+import json
+
+from Meeting.models import *
 
 # Create your tests here.
+
+
+class BaseTestCase(TestCase):
+    def setUp(self):
+        super(TestCase, self).setUp()
+        self.client = Client()
+        self.m = Meeting()
+        self.m.save()
+        self.old_ts = TokenSet(meeting=self.m)
+        self.old_ts.save()
+        self.ts = TokenSet(meeting=self.m)
+        self.ts.save()
+        self.admin = User()
+        self.admin.is_superuser = True
+        self.admin.save()
+
+
+class ManagementInterfaceCases(BaseTestCase):
+    def setUp(self):
+        super(ManagementInterfaceCases, self).setUp()
+        self.client.force_login(self.admin)
+        pass
+
+    def test_create_yna_vote(self):
+        x = Vote.objects.count()
+        result = self.client.post(reverse('meeting/new_vote', args=[self.m.id]), {'name': 'name',
+                        'description':'description',
+                        'method':Vote.YES_NO_ABS})
+        self.assertEqual(x+1, Vote.objects.count())
+        v = Vote.objects.first()
+        self.assertEqual(v.name, 'name')
+        self.assertEqual(v.description, 'description')
+        self.assertEqual(v.method, Vote.YES_NO_ABS)
+        self.assertEqual(v.state, Vote.READY)
+        self.assertEqual(v.token_set, self.ts)
+        self.assertListEqual(list(v.option_set.all().values_list('name', flat=True)), ['abs', 'no', 'yes'])
+
+    def test_create_yna_vote(self):
+        x = Vote.objects.count()
+        result = self.client.post(reverse('meeting/new_vote', args=[self.m.id]), {'name': 'name',
+                        'description':'description',
+                        'method':Vote.STV})
+        self.assertEqual(x+1, Vote.objects.count())
+        v = Vote.objects.first()
+        self.assertEqual(v.name, 'name')
+        self.assertEqual(v.description, 'description')
+        self.assertEqual(v.method, Vote.STV)
+        self.assertEqual(v.state, Vote.READY)
+        self.assertEqual(v.token_set, self.ts)
+        self.assertEqual(v.option_set.count(), 0)
+
+    def test_add_vote_option(self):
+        v = Vote(name='name', token_set=self.ts, method=Vote.YES_NO_ABS)
+        v.save()
+        request_args = [reverse('meeting/add_vote_option', args=[self.m.id, v.id]),
+                        {'name': 'name'}]
+        result = self.client.post(*request_args)
+        self.assertJSONEqual(result.content, json.dumps({'result': 'failure'}))
+        cant_add_option_states = [x[0] for x in Vote.states]
+        cant_add_option_states.remove(Vote.READY)
+        for s in cant_add_option_states:
+            v = Vote(name='name', token_set=self.ts, method=Vote.STV, state=s)
+            v.save()
+            request_args = [reverse('meeting/add_vote_option', args=[self.m.id, v.id]),
+                            {'name': 'name'}]
+            result = self.client.post(*request_args)
+            self.assertJSONEqual(result.content, json.dumps({'result': 'failure'}))
+        v = Vote(name='name', token_set=self.ts, method=Vote.STV)
+        v.save()
+        request_args = [reverse('meeting/add_vote_option', args=[self.m.id, v.id]),
+                        {'name': 'name'}]
+        result = self.client.post(*request_args)
+        self.assertJSONEqual(result.content, json.dumps({'result': 'success',
+                                                         'id': v.option_set.first().id}))
+
+
+class TallyingYNACases(TestCase):
+    def setUp(self):
+        pass
+
+
+
+class TallyingSTVCases(TestCase):
+    def setUp(self):
+        pass
