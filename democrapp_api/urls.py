acec1c3ca0588b67b6f65c503bd83e0121634519
deploy <deploy@deploy.com>
2018-03-06 16:18:54 +0000

    updating settings and urls to reflect nginx config

--- democrapp_api/urls.py
+++ democrapp_api/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',  include('Meeting.urls')),
+    path('api/admin/', admin.site.urls),
+    path('api/',  include('Meeting.urls')),
 ]
