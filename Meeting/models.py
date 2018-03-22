85e074164db0de2187e223ed7f379615f8344dc3
Kyle Cooke <cookekyle99@yahoo.co.uk>
2018-03-22 12:20:33 +0000

    added links from admin site to management pages for meetings

--- Meeting/models.py
+++ Meeting/models.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from django.db import models
+from django.urls import reverse
 import uuid
 import random
 
@@ -22,6 +23,9 @@ class Meeting(models.Model):
 
     def __str__(self):
         return "{} \t-\t {}".format(self.time.date(), self.name)
+
+    def get_absolute_url(self):
+        return reverse('meeting/manage', args=[self.pk])
 
 
 class TokenSet(models.Model):
