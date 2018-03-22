507fe9882471f530a4a8d7c36fb6de842e21cd84
Kyle Cooke <cookekyle99@yahoo.co.uk>
2018-03-22 12:25:48 +0000

    hid meeting close time
    was causing confusion and meetings to be made pre-closed

--- Meeting/admin.py
+++ Meeting/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Meeting)
 admin.site.site_header = 'DemocrApp Administration'
 admin.site.site_title = 'DemocrApp'
 admin.site.index_title = 'DemocrApp Administration'
@@ -20,6 +19,11 @@ class VoteAdmin(admin.ModelAdmin):
     inlines = [OptionInline]
 
 
+class MeetingAdmin(admin.ModelAdmin):
+    fields = ['time', 'name']
+
+
+admin.site.register(Meeting, MeetingAdmin)
 admin.site.register(AuthToken, AuthTokenAdmin)
 admin.site.register(TokenSet)
 admin.site.register(Vote, VoteAdmin)
