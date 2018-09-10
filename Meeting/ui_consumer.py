e69b4069b4081b553a9b81a142cab521326947e4
rphi <8237136+rphi@users.noreply.github.com>
2018-09-10 17:30:32 +0100

    Fix caching of validitity of tokens

--- Meeting/ui_consumer.py
+++ Meeting/ui_consumer.py
@@ -72,6 +72,7 @@ class UIConsumer(JsonWebsocketConsumer):
         vote_num = message['ballot_id']
         vote = Vote.objects.filter(pk=vote_num).first()
         tokens = []
+        self.session.refresh_from_db()
         if self.session.auth_token.valid_for(vote) and vote.state == Vote.LIVE:
             for voter in message['votes'].items():
                 voter_id = int(voter[0])
