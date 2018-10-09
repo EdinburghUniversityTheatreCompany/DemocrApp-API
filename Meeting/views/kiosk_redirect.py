e67c79b28b962ae4c9fc121e2333c17cb9f04ac3
Kyle Cooke <cookekyle99@yahoo.co.uk>
2018-10-09 15:38:30 +0100

    fix kiosk_redirect selecting closed meetings

--- Meeting/views/kiosk_redirect.py
+++ Meeting/views/kiosk_redirect.py
@@ -5,7 +5,7 @@ from Meeting.models import Meeting
 
 
 def kiosk_redirect(request):
-    meeting_query = Meeting.objects.filter(close_time__isnull=False).order_by('-time')
+    meeting_query = Meeting.objects.filter(close_time__isnull=True).order_by('-time')
     if meeting_query.exists():
         meeting = meeting_query.first()
         return redirect('/?k=true&m={}'.format(meeting.id))
